8c6f023ad48277eb87312841bc4d0ba8d8311de0
RBianchi-Al <renatalebianchi@gmail.com>
2021-06-23 10:28:57 -0300

    middlewares

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -6,18 +6,9 @@ class CreateUserController{
     async handle(request: Request, response: Response){
 
         const {name, email, admin} = request.body;
-
         const createUserService = new CreateUserService();
-        try{
-            const user = await createUserService.execute({name, email, admin});
-            return response.json(user)
-        }catch(err){
-            return response.status(400).json({
-                message: err.message
-            })
-        }
-
-    
+        const user = await createUserService.execute({name, email, admin});
+        return response.json(user);    
     
     }  
 }
